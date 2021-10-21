73ba0b5015ac7e35c6b8117ab22517dd53a9de6b
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-21 09:31:51 -0500

    inital commit

--- challenge-is-palindrome/isPalindrome.js
+++ challenge-is-palindrome/isPalindrome.js
@@ -1,6 +1,6 @@
 // Write your code below
 function palindrome(word) {
-    for (let i = 0; i < word.length / 2; i++) {
+    for (let i = 0; i < word.length; i++) {
       if (word[i] !== word[word.length - 1 -i]) {
         return false;
       }
