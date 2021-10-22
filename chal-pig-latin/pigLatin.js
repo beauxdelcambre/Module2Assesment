9970fe2b5dfa1df7f541903ca9bf48b23ae154a6
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-22 13:16:05 -0400

    final commit

--- chal-pig-latin/pigLatin.js
+++ chal-pig-latin/pigLatin.js
@@ -1,2 +1,8 @@
 // Write your solution below:
+function translatePigLatain(str){
+    let firstVowel = str.search(/[aeiou]/);
+    
+    return firstVowel === 0 ? str + "yay" : str.substr(firstVowel) + str.substr(0, firstVowel) + "ay";
+}
 
+console.log(translatePigLatain("Hello world this is pig latin"))
