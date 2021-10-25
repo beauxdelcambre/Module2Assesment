f3661894ce239e30f3f216ae31d3eef0526f9437
beauxdelcambre <91709259+beauxdelcambre@users.noreply.github.com>
2021-10-24 21:49:08 -0500

    Delete chal-pig-latin directory

--- chal-pig-latin/pigLatin.js
+++
@@ -1,8 +0,0 @@
-// Write your solution below:
-function translatePigLatain(str){
-    let firstVowel = str.search(/[aeiou]/);
-    
-    return firstVowel === 0 ? str + "yay" : str.substr(firstVowel) + str.substr(0, firstVowel) + "ay";
-}
-
-console.log(translatePigLatain("Hello world this is pig latin"))
